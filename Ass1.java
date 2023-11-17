37fee3c765f30c214a5ac4533216ef6471abc354
Sam12993a <gragerssam@gmail.com>
2023-11-17 13:06:07 +0530

    Intro to java Assingments

---
+++ Ass1.java
@@ -0,0 +1,19 @@
+public class Ass1 {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 5;
+        int num2 = 10;
+
+        // Using relational operators to compare the numbers
+        boolean isEqual = num1 == num2;
+        boolean notEqual = num1 != num2;
+        boolean greaterThan = num1 > num2;
+        boolean lessThan = num1 < num2;
+
+        // Output the results
+        System.out.println("Equal: " + isEqual);
+        System.out.println("Not Equal: " + notEqual);
+        System.out.println("num1 is Greater Than num2: " + greaterThan);
+        System.out.println("num1 is Less Than num2: " + lessThan);
+    }
+}
---
+++ Ass2.java
@@ -0,0 +1,22 @@
+public class Ass2 {
+    public static void main(String[] args) {
+        // Initialize a variable
+        int num = 10;
+
+        // Using assignment operators to perform operations
+        num += 2; // Equivalent to num = num + 2
+        System.out.println("After addition: num = " + num);
+
+        num -= 4; // Equivalent to num = num - 4
+        System.out.println("After subtraction: num = " + num);
+
+        num *= 7; // Equivalent to num = num * 7
+        System.out.println("After multiplication: num = " + num);
+
+        num /= 4; // Equivalent to num = num / 4
+        System.out.println("After division: num = " + num);
+
+        num %= 3; // Equivalent to num = num % 3
+        System.out.println("After modulus: num = " + num);
+    }
+}
